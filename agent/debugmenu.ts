4f964e1be93a8078a6e7bf5e7e65699f7048b1e0
Natesworks <contact@natesworks.com>
2025-08-10 14:52:36 +0200

    half working

--- agent/debugmenu.ts
+++ agent/debugmenu.ts
@@ -2,25 +2,23 @@ import { addFile, base, customButtonSetMovieClip, gameButtonConstructor, malloc,
 import { Offsets } from "./offsets.js";
 import { createStringObject, strPtr } from "./util.js";
 
-let debugSCPtr: NativePointer;
-
 export function addDebugFile() {
     const adder = Interceptor.attach(base.add(Offsets.ResourceListenerAddFile),
         {
             onEnter(args) {
                 adder.detach();
-                debugSCPtr = Memory.alloc(Process.pointerSize)
-                debugSCPtr.writePointer(base.add(Offsets.DebugSC))
-                addFile(args[0], debugSCPtr, -1, -1, -1, -1, 0);
+                addFile(args[0], createStringObject("sc/debug.sc"), -1, -1, -1, -1, 0);
                 console.log("sc/debug.sc loaded");
             }
         })
 }
 
-export function spawnItem(item: string, text: string, x: number, y: number) : NativePointer {
+export function spawnItem(item: string, text: string, x: number, y: number): NativePointer {
     let mem = malloc(1024);
     gameButtonConstructor(mem);
-    let movieClip = resourceManagerGetMovieClip(debugSCPtr, Memory.allocUtf8String(item), 1);
+    let itemPtr = strPtr(item);
+    let debugSCPtr = strPtr("sc/debug.sc");
+    let movieClip = resourceManagerGetMovieClip(debugSCPtr, itemPtr, 1);
     customButtonSetMovieClip(mem, movieClip);
     movieClipSetText(mem, createStringObject(text));
     return mem;
