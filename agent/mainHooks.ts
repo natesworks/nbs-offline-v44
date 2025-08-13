c71deda8453d1722a2a529c6fab20584b213c019
Natesworks <contact@natesworks.com>
2025-08-13 11:12:21 +0200

    oops

--- agent/mainHooks.ts
+++ agent/mainHooks.ts
@@ -17,7 +17,8 @@ export function installHooks() {
     Interceptor.attach(base.add(Offsets.ServerConnectionUpdate),
         {
             onEnter: function (args) {
-                args[0].add(4).readPointer().add(Offsets.HasConnectFailed).writeU8(0);
+                args[0].add(Process.pointerSize).readPointer().add(Offsets.HasConnectFailed).writeU8(0);
+                args[0].add(Process.pointerSize).readPointer().add(Offsets.State).writeInt(5);
             }
         });
 
