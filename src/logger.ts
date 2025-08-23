20880070328e4aaf59ed901a4632342f50bf4a55
Natesworks <contact@natesworks.com>
2025-08-23 14:04:27 +0200

    Add seconds to log

--- src/logger.ts
+++ src/logger.ts
@@ -20,7 +20,8 @@ function getTimestamp(): string {
     const yy = String(d.getFullYear()).slice(2);
     const hh = String(d.getHours()).padStart(2, '0');
     const mi = String(d.getMinutes()).padStart(2, '0');
-    return `[${dd}/${mm}/${yy} ${hh}:${mi}]`;
+    const ss = String(d.getSeconds()).padStart(2, '0');
+    return `[${dd}/${mm}/${yy} ${hh}:${mi}:${ss}]`;
 }
 
 export class Logger {
